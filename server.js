8fa3a97563938e35f5f6e5f444538872eca6e6ed
SoySourceTan <soy@viscatman.com>
2025-07-06 14:06:16 +0900

    Add files via upload

--- server.js
+++ server.js
@@ -4,14 +4,27 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get('/get-ip', (req, res) => {
-  const ipAddress = req.headers['x-forwarded-for'] || req.socket.remoteAddress || 'unknown';
-  res.json({ ipAddress });
+// CORS対応
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
 });
 
 app.use(express.static(__dirname));
 
+app.get('/get-ip', (req, res) => {
+  const ipAddress = req.headers['x-forwarded-for'] || req.socket.remoteAddress || 'unknown';
+  console.log('GET /get-ip:', { ipAddress, headers: req.headers });
+  res.json({ ipAddress });
+});
+
 app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.get('/learning/english-words/chat/*', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
