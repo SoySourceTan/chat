f1b1ee88741cc8ee1783bdda11db609b430c6b4f
SoySourceTan <soy@viscatman.com>
2025-07-07 22:41:41 +0900

    Add files via upload

---
+++ onlineUsers.js
@@ -0,0 +1,16 @@
+export function createOnlineUsersList(users) {
+  const onlineUsersList = document.createElement('div');
+  onlineUsersList.className = 'online-users-list';
+  onlineUsersList.innerHTML = '<h5>オンラインユーザー</h5>';
+
+  const userList = document.createElement('ul');
+  userList.className = 'list-unstyled';
+  for (const userId in users) {
+    const user = users[userId];
+    const li = document.createElement('li');
+    li.textContent = user.username;
+    userList.appendChild(li);
+  }
+  onlineUsersList.appendChild(userList);
+  return onlineUsersList;
+}
